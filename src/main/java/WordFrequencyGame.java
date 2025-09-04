8502f0ae330667e41be3f4ad65a9be20a934f224
LIJA30 <jade.li@oocl.com>
2025-09-04 14:09:55 +0800

    refactor: remove redundant spaces

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,35 +9,26 @@ import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr){
-
-
         if (inputStr.split("\\s+").length==1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split("\\s+");
-
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
-
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map =getListMap(inputList);
-
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()){
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
                 inputList = list;
-
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
                 StringJoiner joiner = new StringJoiner("\n");
                 for (Input w : inputList) {
                     String s = w.getValue() + " " +w.getWordCount();
@@ -45,8 +36,6 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
@@ -62,15 +51,10 @@ public class WordFrequencyGame {
                 arr.add(input);
                 map.put(input.getValue(), arr);
             }
-
             else {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
-
-
 }
