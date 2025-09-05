8f138a9f6adfa4b3e72ba4cc09cfc662154025ee
LIJA30 <jade.li@oocl.com>
2025-09-05 11:42:07 +0800

    refactor: optimized FizzBuzz function if else if

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,10 @@
 public class FizzBuzz {
     public static String countOff(int order) {
         if ((order % 3) != 0 && (order % 5) != 0) {
-            return "%d".formatted(order);
-        }
-        if ((order % 15) == 0) {
+            return Integer.toString(order);
+        } else if ((order % 15) == 0) {
             return "FizzBuzz";
-        }
-        if ((order % 3) == 0) {
+        } else if ((order % 3) == 0) {
             return "Fizz";
         }
         return "Buzz";
